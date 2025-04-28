567e997f5e00e08884cda36b7bc1802d6947cf19
Joas A Santos <34966120+CyberSecurityUP@users.noreply.github.com>
2025-04-28 03:02:07 -0300

    Update token.h

--- Linux/include/token.h
+++ Linux/include/token.h
@@ -4,35 +4,27 @@
 #include <string>
 
 enum class TokenType {
-    // Palavras-chave
     Let, If, Else, While, Func, Return, True, False, Print, Input, Break,
     Continue, Struct, Class, This,
 
-    // Identificadores e literais
     Identifier, Number, String,
 
-    // Operadores
     Plus, Minus, Star, Slash,
     Equal, EqualEqual, BangEqual,
     Less, LessEqual, Greater, GreaterEqual,
     And, Or, Not, AndAnd, OrOr,
     
-    // === Estruturas avançadas ===
     Enum, Match, Switch, Arrow, Case, Default,
 
-    // Símbolos
     LParen, RParen,
     LBrace, RBrace,
     Comma, Semicolon, Assign, LBracket, RBracket, For, In,
 
-    // Especial
     Eof, Invalid,
 
-    // Casting
     Int, Float, Str, Bool, Dot,
     
-    // Tipos para input<T>
-    IntType, FloatType, BoolType, StrType  // <<< novos tipos
+    IntType, FloatType, BoolType, StrType  
   
 };
 
