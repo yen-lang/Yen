5923739f7663e1deb770d436ccbdc93b7c6bd08b
Joas A Santos <34966120+CyberSecurityUP@users.noreply.github.com>
2026-02-13 18:05:01 -0300

    Add files via upload

---
+++ build/CMakeFiles/yen.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/opt/Yen/src/compiler.cpp" "CMakeFiles/yen.dir/src/compiler.cpp.o" "gcc" "CMakeFiles/yen.dir/src/compiler.cpp.o.d"
+  "/opt/Yen/src/lexer.cpp" "CMakeFiles/yen.dir/src/lexer.cpp.o" "gcc" "CMakeFiles/yen.dir/src/lexer.cpp.o.d"
+  "/opt/Yen/src/main.cpp" "CMakeFiles/yen.dir/src/main.cpp.o" "gcc" "CMakeFiles/yen.dir/src/main.cpp.o.d"
+  "/opt/Yen/src/native_libs.cpp" "CMakeFiles/yen.dir/src/native_libs.cpp.o" "gcc" "CMakeFiles/yen.dir/src/native_libs.cpp.o.d"
+  "/opt/Yen/src/parser.cpp" "CMakeFiles/yen.dir/src/parser.cpp.o" "gcc" "CMakeFiles/yen.dir/src/parser.cpp.o.d"
+  "/opt/Yen/src/stdlib.cpp" "CMakeFiles/yen.dir/src/stdlib.cpp.o" "gcc" "CMakeFiles/yen.dir/src/stdlib.cpp.o.d"
+  "/opt/Yen/src/type_checker.cpp" "CMakeFiles/yen.dir/src/type_checker.cpp.o" "gcc" "CMakeFiles/yen.dir/src/type_checker.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
